e10e04a2134c56ce2c6faf6df8080275df4a7cc7
Emiliano Cabrera Pérez <57124413+ECabrera3094@users.noreply.github.com>
2024-06-14 17:06:12 -0600

    Add files via upload

---
+++ Engagement_ClaroVideo/test_main.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page
+
+from TestCases.test_engagement import Engagement
+
+def test_basic_search(page: Page):
+
+    send_email = Engagement(page)
+    send_email.navigate()
+    send_email.engagement_ClaroVideo()
+    send_email.engagement_ClaroMusica()
+    send_email.engagement_ClaroDrive()
+    send_email.engagement_ClaroConnect()
+
+# python -m pytest main.py --html=reporte.html
+
+# https://pypi.org/project/pytest-html-reporter/
