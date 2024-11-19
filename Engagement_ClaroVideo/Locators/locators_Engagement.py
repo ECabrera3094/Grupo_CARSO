d94b2cecceda917be4e031e59fbd6a507b70c477
Emiliano Cabrera Pérez <57124413+ECabrera3094@users.noreply.github.com>
2024-11-19 12:40:42 -0600

    Add files via upload

--- Engagement_ClaroVideo/Locators/locators_Engagement.py
+++ Engagement_ClaroVideo/Locators/locators_Engagement.py
@@ -3,3 +3,9 @@ class Locators_Engagement:
     URL = "https://engagement-east.clarovideo.net/v1/queue/paneles/mail-test?authpn=M41L3R&authpt=L03LL7"
 
     email = "pruebasl735@gmail.com"
+
+    variables = {"NAME":"Prueba",
+                "PRECIO":"440",
+                "DATE":"13/12/2024",
+                "PROMO_DESC":"Suscripción Universal Plus"
+                }
