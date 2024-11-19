c7c50abc234f8821c8d46eb75d61f733fe91b284
Emiliano Cabrera Pérez <57124413+ECabrera3094@users.noreply.github.com>
2024-11-19 12:40:21 -0600

    Add files via upload

--- Engagement_ClaroVideo/TestCases/test_engagement.py
+++ Engagement_ClaroVideo/TestCases/test_engagement.py
@@ -1,3 +1,4 @@
+#import json
 import time
 from playwright.sync_api import Page
 
@@ -9,6 +10,7 @@ class Engagement:
         self.page = page
         self.URL = Locators_Engagement.URL
         self.email = Locators_Engagement.email
+        self.variables = Locators_Engagement.variables
 
     def navigate(self):
         self.page.goto(self.URL)
@@ -17,37 +19,39 @@ class Engagement:
 
         self.page.locator("id=emails").fill(self.email)
 
-        self.page.screenshot(path="ClaroVideo_screenshot01.png")
-
         self.page.locator("id=keys").fill('cv-welcome\ncv-passwordreminder\ncv-change-password-success\ncv-activatecontrolpin\ncv-remembercontrolpin\ncv-newlogin-notification')
+        #self.page.locator("id=keys").fill('email_susc_telmexmexico_abono_universal_plus_uruguay_amcogate_0\nemail_susc_telmexmexico_abono_universal_plus_uruguay_hubgate_0\nemail_susc_telmexmexico_abono_universal_plus_uruguay_promogate_0\nemail_susc_telmexmexico_abono_universal_plus_uruguay_hubgate_0_70007061\nemail_cancel_telmexmexico_abono_universal_plus_uruguay_hubgate_1200\nemail_cancel_telmexmexico_abono_universal_plus_uruguay_amcogate_1200\nemail_cancel_telmexmexico_abono_universal_plus_uruguay_promogate_1200')
 
         self.page.locator("id=regions").fill("MX")
+        #self.page.locator("id=regions").fill("UY")
 
         self.page.locator("id=partner").select_option("CLAROVIDEO")
-
-        self.page.screenshot(path="ClaroVideo_screenshot02.png")
+        
+        '''
+        Convertir el diccionario a una cadena JSON con Comillas Dobles.
+        La función json.dumps() en Python convierte un objeto de Python (como un diccionario) en una cadena de texto en formato JSON. 
+        Al establecer ensure_ascii=False, indicas que los caracteres especiales deben mostrarse tal como están, sin convertirlos a secuencias de escape.
+        '''
+        #json_string_variables = json.dumps(self.variables, ensure_ascii=False)
+        #self.page.locator("id=variables").fill(json_string_variables)
 
         time.sleep(3)
 
         self.page.locator("id=sender").click(force = True)
 
         time.sleep(10)
-
+        
         self.page.screenshot(path="ClaroVideo_screenshot03.png")
 
     def engagement_ClaroMusica(self):
 
         self.page.locator("id=emails").fill(self.email)
 
-        self.page.screenshot(path="ClaroMusica_screenshot01.png")
-
         self.page.locator("id=keys").fill("cm-welcome-jwt")
 
         self.page.locator("id=regions").fill("MX")
 
         self.page.locator("id=partner").select_option("CLAROMUSICA")
-
-        self.page.screenshot(path="ClaroMusica_screenshot02.png")
 
         time.sleep(3)
 
@@ -61,15 +65,11 @@ class Engagement:
         
         self.page.locator("id=emails").fill(self.email)
 
-        self.page.screenshot(path="ClaroDrive_screenshot01.png")
-
-        self.page.locator("id=keys").fill("cb_welcome-wsgu\ncd-recovery-password")
+        self.page.locator("id=keys").fill("cb_welcome\ncd-recovery-password")
 
         self.page.locator("id=regions").fill("MX")
 
         self.page.locator("id=partner").select_option("CLARODRIVE")
-
-        self.page.screenshot(path="ClaroDrive_screenshot02.png")
 
         time.sleep(3)
 
@@ -83,15 +83,11 @@ class Engagement:
 
         self.page.locator("id=emails").fill(self.email)
 
-        self.page.screenshot(path="ClaroConnect_screenshot01.png")
-
         self.page.locator("id=keys").fill("cc-emails-remember\ncc-emails-invite\ncc-emails-forgotpw\ncc-emails-cancel")
 
         self.page.locator("id=regions").fill("MX")
 
         self.page.locator("id=partner").select_option("CLAROCONNECT")
-
-        self.page.screenshot(path="ClaroConnect_screenshot02.png")
 
         time.sleep(3)
 
