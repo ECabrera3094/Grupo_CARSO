1cc5d5e223456d8130bd1f0a0ae5c99368f4dbb7
Emiliano Cabrera Pérez <57124413+ECabrera3094@users.noreply.github.com>
2024-12-17 20:18:05 -0600

    Add files via upload

--- Plan_Brasil/Queries/queries_Plan_Brasil.py
+++ Plan_Brasil/Queries/queries_Plan_Brasil.py
@@ -3,7 +3,14 @@ from google.cloud import bigquery
 def query_Brasil(table, var_country, country, var_date, date):
     
     alias = table.split('.')[-1]
-    
+
+    # Estas variables se encargan de formatear los valores de country y date según su tipo (str o int) antes de incluirlos en la consulta SQL.
+    # verifica si las variables country y date son cadenas de texto (str). Si lo son, se envuelven entre comillas simples. 
+    # Si no lo son (por ejemplo, son enteros), se convierten a cadenas sin envolverlas.
+    # isinstance(country, str): Esta función verifica si country es una cadena (str).
+    formatted_country = f"'{country}'" if isinstance(country, str) else str(country)
+    formatted_date = f"'{date}'" if isinstance(date, str) else str(date)
+
     # Crea un cliente de BigQuery
     #client = bigquery.Client(project="amco-cv-prod") # Solo aplica a Proyectos en la Nube.
 
@@ -18,8 +25,8 @@ def query_Brasil(table, var_country, country, var_date, date):
                 WHEN EXISTS (
                     SELECT 1 
                     FROM {table}
-                    WHERE {var_country} = '{country}'
-                    AND {var_date} <= '{date}'
+                    WHERE {var_country} = {formatted_country}
+                    AND {var_date} <= {formatted_date}
                 ) THEN 1 
                 ELSE 0 
             END AS {alias}_BRASIL;
@@ -31,9 +38,9 @@ def query_Brasil(table, var_country, country, var_date, date):
 
     row = next(results)
 
-    print(row)
+    print("Row: ", row)
 
     valor = row[0]
 
-    # Mostrar valor
-    print("Valor: ", valor ) 
+    # Mostrar Resultado
+    print("Resultado: ", valor) 
